d7f39fc0316c373c5e3fa76f7b09ede09b9edb69
zaraknyu <razenxizu@gmail.com>
2026-09-09 19:29:04 +0000

    mumba: Drop patching fpc fingerprint service
    file expected to be fixed up, but pre-fixup hash and post-fixup hash are the same
    
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -132,9 +132,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
     'vendor/lib64/libaudio_aidl_conversion_common_ndk_prebuilt.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-ndk.so', 'android.media.audio.common.types-V3-ndk.so'),
-    'vendor/bin/hw/android.hardware.biometrics.fingerprint-service.fpc': blob_fixup()
-        .remove_needed('android.hardware.biometrics.fingerprint-V4-ndk.so')
-        .remove_needed('android.hardware.biometrics.common-V4-ndk.so'),
     ('system_ext/etc/seccomp_policy/tcmd.policy', 'vendor/etc/seccomp_policy/qsap_qapeservice.policy', 'vendor/etc/seccomp_policy/syshealthmon.policy'): blob_fixup()
         .add_line_if_missing('lseek: 1'),
     (
