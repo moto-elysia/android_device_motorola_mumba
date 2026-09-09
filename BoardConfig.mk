83b0d4fe6a4146bbe00fa48d65c796efd746f6fc
zaraknyu <razenxizu@gmail.com>
2026-09-09 18:59:15 +0000

    mumba: Switch dolby sepolicy to hardware/motorola
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
